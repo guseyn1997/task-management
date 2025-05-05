f7103cc8a0ec195ccd80a1a581aae68cd7649233
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:47:25 +0200

    Update tasks.py

--- app/crud/tasks.py
+++ app/crud/tasks.py
@@ -1,1 +1,13 @@
+from sqlalchemy.orm import Session
+from app.models import Task
+from app.schemas import TaskCreate
 
+class CRUDTask:
+    def create(self, db: Session, obj_in: TaskCreate):
+        db_obj = Task(**obj_in.dict())
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+task = CRUDTask()
