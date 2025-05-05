c8235b3ad27f8f5c51d50918391c363c844a020e
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:56:35 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,13 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str
     secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    database_url: str
 
     class Config:
         env_file = ".env"
+        case_sensitive = False
 
 settings = Settings()
