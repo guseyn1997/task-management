9cc6f1cd3cc6f23115d8c546b289a6b40dbd0e83
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:47:13 +0200

    Update projects.py

--- app/crud/projects.py
+++ app/crud/projects.py
@@ -1,1 +1,13 @@
+from sqlalchemy.orm import Session
+from app.models import Project
+from app.schemas import ProjectCreate
 
+class CRUDProject:
+    def create(self, db: Session, obj_in: ProjectCreate, owner_id: int):
+        db_obj = Project(**obj_in.dict(), owner_id=owner_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+project = CRUDProject()
