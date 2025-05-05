669ff85a00e66ebab381d1c9b0ed75de48084fcd
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:35:39 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from app.api import users, auth
+from app.database import engine
+from app.models import Base
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(users.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Task Management API"}
