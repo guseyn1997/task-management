347630ea15c72c1dbbb68206732e2c954d6eeb1d
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:52:02 +0200

    Update project.py

--- app/models/project.py
+++ app/models/project.py
@@ -4,10 +4,11 @@ from app.database import Base
 
 class Project(Base):
     __tablename__ = "projects"
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), index=True)
-    description = Column(String(500))
+    title = Column(String, index=True)
+    description = Column(String)
     owner_id = Column(Integer, ForeignKey("users.id"))
-    
+
+    owner = relationship("User", back_populates="projects")
     tasks = relationship("Task", back_populates="project")
