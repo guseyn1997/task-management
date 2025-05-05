7de6fd948881eea874b7fbd3527f8c243cbe6876
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:46:34 +0200

    Update project.py

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+from datetime import datetime
 
+class ProjectBase(BaseModel):
+    title: str
+    description: str | None = None
+
+class ProjectCreate(ProjectBase):
+    pass
+
+class Project(ProjectBase):
+    id: int
+    owner_id: int
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True
