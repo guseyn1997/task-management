378eef75ecde7ce0947e188865ad0ca68a9084be
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:46:49 +0200

    Update task.py

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
+from datetime import datetime
 
+class TaskBase(BaseModel):
+    title: str
+    description: str | None = None
+    priority: int = 3
+
+class TaskCreate(TaskBase):
+    project_id: int
+
+class Task(TaskBase):
+    id: int
+    is_completed: bool
+    created_at: datetime
+    assignee_id: int | None
+    
+    class Config:
+        orm_mode = True
