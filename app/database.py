2fb77a2bcd25fc686bc4663f152fee45fb782929
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:50:24 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,13 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# Dependency для FastAPI
 def get_db():
     db = SessionLocal()
     try:
