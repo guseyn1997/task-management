7adb8cc237876ceecf7a7ef4c3ded5ea5bdb5b2b
guseyn1997 <gus.1997.mail@gmail.com>
2025-05-05 16:50:56 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -14,7 +14,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency для FastAPI
 def get_db():
     db = SessionLocal()
     try:
